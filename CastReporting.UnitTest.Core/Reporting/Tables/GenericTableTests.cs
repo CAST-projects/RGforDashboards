8f86e0f213840cdcef05873508ab2dae43638cdb
AEteve <a.eteve@castsoftware.com>
2019-09-19 13:55:52 +0200

    REPORTGEN-348: porting to ReportGeneratorForDashboard

--- CastReporting.UnitTest.Core/Reporting/Tables/GenericTableTests.cs
+++ CastReporting.UnitTest.Core/Reporting/Tables/GenericTableTests.cs
@@ -908,5 +908,196 @@ namespace CastReporting.UnitTest.Reporting.Tables
             expectedData.AddRange(new List<string> { "    Snap_v1.1.3 - v1.1.3", "n/a", "3.87" });
             TestUtility.AssertTableContent(table, expectedData, 3, 13);
         }
+
+        [TestMethod]
+        [DeploymentItem(@".\Data\DreamTeamSnap4Metrics2.json", "Data")]
+        [DeploymentItem(@".\Data\DreamTeamSnap4Metrics2Previous.json", "Data")]
+        public void TestCustomExpressionsForApplication()
+        {
+            ReportData reportData = TestUtility.PrepaReportData("AppliAEP",
+                null, @".\Data\DreamTeamSnap4Metrics2.json", "AED3/applications/3/snapshots/4", "Snap_v1.1.4", "v1.1.4",
+                null, @".\Data\DreamTeamSnap4Metrics2Previous.json", "AED3/applications/3/snapshots/3", "Snap_v1.1.3", "v1.1.3");
+            var component = new GenericTable();
+            Dictionary<string, string> config = new Dictionary<string, string>
+            {
+                {"COL1", "CUSTOM_EXPRESSIONS"},
+                {"ROW1", "SNAPSHOTS"},
+                {"CUSTOM_EXPRESSIONS", "a/b|(c+d)/2"},
+                {"PARAMS", "Sz a SZ b QR c QR d"},
+                {"a", "10151"},
+                {"b", "10202"},
+                {"c", "550"},
+                {"d", "556"},
+                {"SNAPSHOTS", "CURRENT|PREVIOUS"}
+            };
+            WSConnection connection = new WSConnection
+            {
+                Url = "http://tests/CAST-RESTAPI/rest/",
+                Login = "admin",
+                Password = "cast",
+                IsActive = true,
+                Name = "Default"
+            };
+            reportData.SnapshotExplorer = new SnapshotBLLStub(connection, reportData.CurrentSnapshot);
+            reportData.RuleExplorer = new RuleBLLStub();
+            var table = component.Content(reportData, config);
+            var expectedData = new List<string>();
+            expectedData.AddRange(new List<string> {"Snapshots", "a/b", "(c+d)/2"});
+            expectedData.AddRange(new List<string> {"Snap_v1.1.4 - v1.1.4", "92.30", "3.89"});
+            expectedData.AddRange(new List<string> {"Snap_v1.1.3 - v1.1.3", "103.81", "2.82"});
+            TestUtility.AssertTableContent(table, expectedData, 3, 3);
     }
+
+        [TestMethod]
+        [DeploymentItem(@".\Data\ModulesDreamTeam.json", "Data")]
+        [DeploymentItem(@".\Data\DreamTeamSnap15MetricsCustom.json", "Data")]
+        [DeploymentItem(@".\Data\DreamTeamSnap3MetricsCustom.json", "Data")]
+        public void TestCustomExpressionsForModules()
+        {
+            ReportData reportData = TestUtility.PrepaReportData("Dream Team",
+                @".\Data\ModulesDreamTeam.json", @".\Data\DreamTeamSnap15MetricsCustom.json", "AED3/applications/7/snapshots/15", "ADGAutoSnap_Dream Team_4", "4",
+                @".\Data\ModulesDreamTeam.json", @".\Data\DreamTeamSnap3MetricsCustom.json", "AED3/applications/7/snapshots/3", "ADGAutoSnap_Dream Team_1", "1");
+            var component = new GenericTable();
+            Dictionary<string, string> config = new Dictionary<string, string>
+            {
+                {"COL1", "CUSTOM_EXPRESSIONS"},
+                {"ROW1", "MODULES"},
+                {"CUSTOM_EXPRESSIONS", "a/b|(c+d)/2"},
+                {"PARAMS", "Sz a SZ b QR c QR d"},
+                {"a", "67211"},
+                {"b", "10151"},
+                {"c", "60013"},
+                {"d", "60014"},
+                {"MODULES", "ALL"},
+                {"SNAPSHOTS", "CURRENT"}
+            };
+            WSConnection connection = new WSConnection
+            {
+                Url = "http://tests/CAST-RESTAPI/rest/",
+                Login = "admin",
+                Password = "cast",
+                IsActive = true,
+                Name = "Default"
+            };
+            reportData.SnapshotExplorer = new SnapshotBLLStub(connection, reportData.CurrentSnapshot);
+            reportData.RuleExplorer = new RuleBLLStub();
+            var table = component.Content(reportData, config);
+            var expectedData = new List<string>();
+            expectedData.AddRange(new List<string> {"Modules", "a/b", "(c+d)/2"});
+            expectedData.AddRange(new List<string> {"Adg", "0.44", "2.27"});
+            expectedData.AddRange(new List<string> {"Central", "No data found", "No data found"});
+            expectedData.AddRange(new List<string> {"DssAdmin", "0.22", "3.31"});
+            expectedData.AddRange(new List<string> {"Pchit", "0.15", "2.80"});
+            TestUtility.AssertTableContent(table, expectedData, 3, 5);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@".\Data\ModulesDreamTeam.json", "Data")]
+        [DeploymentItem(@".\Data\DreamTeamSnap15MetricsCustom.json", "Data")]
+        [DeploymentItem(@".\Data\DreamTeamSnap3MetricsCustom.json", "Data")]
+        public void TestCustomExpressionsForTechnologies()
+        {
+            ReportData reportData = TestUtility.PrepaReportData("Dream Team",
+                @".\Data\ModulesDreamTeam.json", @".\Data\DreamTeamSnap15MetricsCustom.json", "AED3/applications/7/snapshots/15", "ADGAutoSnap_Dream Team_4", "4",
+                @".\Data\ModulesDreamTeam.json", @".\Data\DreamTeamSnap3MetricsCustom.json", "AED3/applications/7/snapshots/3", "ADGAutoSnap_Dream Team_1", "1");
+            reportData.CurrentSnapshot.Technologies = new[] { "JEE", "PL/SQL", "C++", ".NET" };
+            var component = new GenericTable();
+            Dictionary<string, string> config = new Dictionary<string, string>
+            {
+                {"COL1", "CUSTOM_EXPRESSIONS"},
+                {"ROW1", "TECHNOLOGIES"},
+                {"CUSTOM_EXPRESSIONS", "a/b|(c+d)/2"},
+                {"PARAMS", "Sz a SZ b QR c QR d"},
+                {"a", "67211"},
+                {"b", "10151"},
+                {"c", "60013"},
+                {"d", "60014"},
+                {"TECHNOLOGIES", "ALL"},
+                {"SNAPSHOTS", "CURRENT"}
+            };
+            WSConnection connection = new WSConnection
+            {
+                Url = "http://tests/CAST-RESTAPI/rest/",
+                Login = "admin",
+                Password = "cast",
+                IsActive = true,
+                Name = "Default"
+            };
+            reportData.SnapshotExplorer = new SnapshotBLLStub(connection, reportData.CurrentSnapshot);
+            reportData.RuleExplorer = new RuleBLLStub();
+            var table = component.Content(reportData, config);
+            var expectedData = new List<string>();
+            expectedData.AddRange(new List<string> {"Technologies", "a/b", "(c+d)/2"});
+            expectedData.AddRange(new List<string> {"JEE", "0.44", "2.27"});
+            expectedData.AddRange(new List<string> {"PL/SQL", "No data found", "No data found"});
+            expectedData.AddRange(new List<string> {"C++", "0.22", "3.31"});
+            expectedData.AddRange(new List<string> {".NET", "0.15", "2.80"});
+            TestUtility.AssertTableContent(table, expectedData, 3, 5);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@".\Data\ModulesDreamTeam.json", "Data")]
+        [DeploymentItem(@".\Data\DreamTeamSnap15MetricsCustom.json", "Data")]
+        [DeploymentItem(@".\Data\DreamTeamSnap3MetricsCustom.json", "Data")]
+        public void TestCustomExpressionsForModulesAndTechnologies()
+        {
+            ReportData reportData = TestUtility.PrepaReportData("Dream Team",
+                @".\Data\ModulesDreamTeam.json", @".\Data\DreamTeamSnap15MetricsCustom.json", "AED3/applications/7/snapshots/15", "ADGAutoSnap_Dream Team_4", "4",
+                @".\Data\ModulesDreamTeam.json", @".\Data\DreamTeamSnap3MetricsCustom.json", "AED3/applications/7/snapshots/3", "ADGAutoSnap_Dream Team_1", "1");
+            reportData.CurrentSnapshot.Technologies = new[] { "JEE", "PL/SQL", "C++", ".NET" };
+            reportData.PreviousSnapshot.Technologies = new[] { "JEE", "PL/SQL", "C++", ".NET" };
+            var component = new GenericTable();
+            Dictionary<string, string> config = new Dictionary<string, string>
+            {
+                {"COL1", "CUSTOM_EXPRESSIONS"},
+                {"ROW1", "MODULES"},
+                {"ROW11", "TECHNOLOGIES"},
+                {"CUSTOM_EXPRESSIONS", "a/b|(c+d)/2"},
+                {"PARAMS", "Sz a SZ b QR c QR d"},
+                {"a", "67211"},
+                {"b", "10151"},
+                {"c", "60013"},
+                {"d", "60014"},
+                {"TECHNOLOGIES", "ALL"},
+                {"MODULES", "ALL"},
+                {"SNAPSHOTS", "PREVIOUS"}
+            };
+            WSConnection connection = new WSConnection
+            {
+                Url = "http://tests/CAST-RESTAPI/rest/",
+                Login = "admin",
+                Password = "cast",
+                IsActive = true,
+                Name = "Default"
+            };
+            reportData.SnapshotExplorer = new SnapshotBLLStub(connection, reportData.CurrentSnapshot);
+            reportData.RuleExplorer = new RuleBLLStub();
+            var table = component.Content(reportData, config);
+            var expectedData = new List<string>();
+            expectedData.AddRange(new List<string> { "Modules", "a/b", "(c+d)/2" });
+            expectedData.AddRange(new List<string> { "Adg", " ", " " });
+            expectedData.AddRange(new List<string> { "    JEE", "0.45", "2.27" });
+            expectedData.AddRange(new List<string> { "    PL/SQL", "No data found", "No data found" });
+            expectedData.AddRange(new List<string> { "    C++", "No data found", "No data found" });
+            expectedData.AddRange(new List<string> { "    .NET", "No data found", "No data found" });
+            expectedData.AddRange(new List<string> { "Central", " ", " " });
+            expectedData.AddRange(new List<string> { "    JEE", "No data found", "No data found" });
+            expectedData.AddRange(new List<string> { "    PL/SQL", "No data found", "No data found" });
+            expectedData.AddRange(new List<string> { "    C++", "No data found", "No data found" });
+            expectedData.AddRange(new List<string> { "    .NET", "No data found", "No data found" });
+            expectedData.AddRange(new List<string> { "DssAdmin", " ", " " });
+            expectedData.AddRange(new List<string> { "    JEE", "No data found", "No data found" });
+            expectedData.AddRange(new List<string> { "    PL/SQL", "No data found", "No data found" });
+            expectedData.AddRange(new List<string> { "    C++", "0.22", "3.32" });
+            expectedData.AddRange(new List<string> { "    .NET", "No data found", "No data found" });
+            expectedData.AddRange(new List<string> { "Pchit", " ", " " });
+            expectedData.AddRange(new List<string> { "    JEE", "No data found", "No data found" });
+            expectedData.AddRange(new List<string> { "    PL/SQL", "No data found", "No data found" });
+            expectedData.AddRange(new List<string> { "    C++", "No data found", "No data found" });
+            expectedData.AddRange(new List<string> { "    .NET", "0.18", "2.75" });
+            TestUtility.AssertTableContent(table, expectedData, 3, 21);
+        }
+
 }
+}
+
