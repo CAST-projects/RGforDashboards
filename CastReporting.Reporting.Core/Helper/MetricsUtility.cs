1a017e38fe1461a3123f3a28a7f808d9420af814
Aurore Eteve <a.eteve@castsoftware.com>
2019-11-15 15:37:58 +0100

    REPORTGEN-348: fix double.parse issue in french

--- CastReporting.Reporting.Core/Helper/MetricsUtility.cs
+++ CastReporting.Reporting.Core/Helper/MetricsUtility.cs
@@ -872,7 +872,7 @@ namespace CastReporting.Reporting.Helper
             DataTable dt = new DataTable();
             try
             {
-                double? res = double.Parse(dt.Compute(expr, "").ToString());
+                double? res = double.Parse(dt.Compute(expr, "").ToString(), System.Globalization.CultureInfo.CurrentCulture);
                 return res.Value.Equals(double.NaN) ? Labels.NoData : res.Value.ToString(metricFormat);
             }
             catch (EvaluateException e)
@@ -888,7 +888,7 @@ namespace CastReporting.Reporting.Helper
             DataTable dt = new DataTable();
             try
             {
-                return double.Parse(dt.Compute(expr, "").ToString());
+                return double.Parse(dt.Compute(expr, "").ToString(), System.Globalization.CultureInfo.CurrentCulture);
             }
             catch (EvaluateException e)
             {
